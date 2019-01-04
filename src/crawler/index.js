38c116213f902163ba3d2acb7a38990041e2b1ee
Elliot Ng <chihimng.io@gmail.com>
2019-01-04 23:08:06 +0800

    fix: better login flow detection

--- src/crawler/index.js
+++ src/crawler/index.js
@@ -4,13 +4,13 @@ import _ from 'lodash'
 import HttpError from 'http-errors'
 
 const URL_SIS_LOGIN = 'https://sisprod.psft.ust.hk/psp/SISPROD/EMPLOYEE/HRMS/c/SA_LEARNER_SERVICES.SSS_STUDENT_CENTER.GBL?pslnkid=Z_HC_SSS_STUDENT_CENTER_LNK&FolderPath=PORTAL_ROOT_OBJECT.Z_HC_SSS_STUDENT_CENTER_LNK&IsFolder=false&IgnoreParamTempl=FolderPath%2cIsFolder'
-const URL_CAS_LOGIN = 'https://cas.ust.hk/cas/login?service=https%3A%2F%2Flogin.psft.ust.hk%2Fcas%2Flogin%3Fclient_name%3D%2522CAS%2BPSFT%2522'
+const URL_CAS_2FA = 'https://cas.ust.hk/cas/js/duo/Duo-Web-v2.min.js'
 const URL_SIS_HOME = 'https://sisprod.psft.ust.hk/psc/SISPROD/EMPLOYEE/HRMS/c/SA_LEARNER_SERVICES.SSS_STUDENT_CENTER.GBL?pslnkid=Z_HC_SSS_STUDENT_CENTER_LNK&FolderPath=PORTAL_ROOT_OBJECT.Z_HC_SSS_STUDENT_CENTER_LNK&IsFolder=false&IgnoreParamTempl=FolderPath%2cIsFolder&PortalActualURL=https%3a%2f%2fsisprod.psft.ust.hk%2fpsc%2fSISPROD%2fEMPLOYEE%2fHRMS%2fc%2fSA_LEARNER_SERVICES.SSS_STUDENT_CENTER.GBL%3fpslnkid%3dZ_HC_SSS_STUDENT_CENTER_LNK&PortalContentURL=https%3a%2f%2fsisprod.psft.ust.hk%2fpsc%2fSISPROD%2fEMPLOYEE%2fHRMS%2fc%2fSA_LEARNER_SERVICES.SSS_STUDENT_CENTER.GBL%3fpslnkid%3dZ_HC_SSS_STUDENT_CENTER_LNK&PortalContentProvider=HRMS&PortalCRefLabel=Student%20Center&PortalRegistryName=EMPLOYEE&PortalServletURI=https%3a%2f%2fsisprod.psft.ust.hk%2fpsp%2fSISPROD%2f&PortalURI=https%3a%2f%2fsisprod.psft.ust.hk%2fpsc%2fSISPROD%2f&PortalHostNode=HRMS&NoCrumbs=yes'
 const URL_SIS_GRADES = 'https://sisprod.psft.ust.hk/psc/SISPROD/EMPLOYEE/HRMS/c/SA_LEARNER_SERVICES.SSR_SSENRL_GRADE.GBL'
 const URL_STU_PROG_INFO = 'https://w5.ab.ust.hk/jsga/graduation'
 const URL_COOKIE_SISPROD = 'https://sisprod.psft.ust.hk'
-const URL_COOKIE_LOGIN_PSFT = 'https://login.psft.ust.hk'
-const URL_COOKIE_CAS = 'https://cas.ust.hk/cas/'
+const URL_COOKIE_LOGIN_PSFT = 'https://login.psft.ust.hk/cas'
+const URL_COOKIE_CAS = 'https://cas.ust.hk/cas'
 const URL_COOKIE_2FA = 'https://api-84f626fe.duosecurity.com'
 const URLS_COOKIE = [URL_COOKIE_SISPROD, URL_COOKIE_LOGIN_PSFT, URL_COOKIE_CAS, URL_COOKIE_2FA]
 const TIMEOUT_2FA = 3 * 60 * 1000
@@ -27,17 +27,22 @@ export async function login (page, username, password, cookies) {
   const SELECTOR_LOGIN_FORM = 'form[id="fm1"]'
   const SELECTOR_SIS_HOME = `frame[src="${URL_SIS_HOME}"]`
   try {
+    await page.setCookie(...cookies)
+    await Promise.all([page.goto(URL_SIS_LOGIN), Promise.race([page.waitFor(SELECTOR_LOGIN_FORM), page.waitFor(SELECTOR_SIS_HOME)])])
+    // FIXME: program info cookie not working, better cookie validation needed
+    if (await page.$(SELECTOR_LOGIN_FORM) !== null) {
+      await page.focus('#userNameInput')
+      await page.keyboard.type(username)
+      await page.focus('#passwordInput')
+      await page.keyboard.type(password)
+      await page.click('#submitButton')
+      const response = await Promise.race([page.waitForResponse(URL_CAS_2FA), page.waitForResponse(res => res.status() === 401)])
+      if (response.status() === 401) { throw new HttpError(401) }
+      // FIXME: 2fa cookie not working, button disabled / selector not working
+      // await page.click('.stay-logged-in input[name="dampen_choice"]')
+      await page.waitFor(SELECTOR_SIS_HOME, { timeout: TIMEOUT_2FA })
+    }
     // FIXME: cas cookie not working, seems can't be captured
-    // await page.setCookie(...cookies)
-    await Promise.all([page.goto(URL_SIS_LOGIN), Promise.race([page.waitFor(SELECTOR_LOGIN_FORM), page.waitForResponse(URL_SIS_HOME)])])
-    if (await page.$(SELECTOR_SIS_HOME) !== null) { return await page.cookies(...URLS_COOKIE) }
-    await page.focus('#userNameInput')
-    await page.keyboard.type(username)
-    await page.focus('#passwordInput')
-    await page.keyboard.type(password)
-    await page.click('#submitButton')
-    const response = await Promise.race([page.waitForResponse(URL_SIS_HOME, { timeout: TIMEOUT_2FA }), page.waitForResponse(res => res.status() === 401)])
-    if (response.status() === 401) { throw new HttpError(401) }
     return await page.cookies(...URLS_COOKIE)
   } catch (e) {
     throw e
